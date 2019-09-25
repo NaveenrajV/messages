da88bf6b7d8ad30f3f0a78c1ca952a4cf0e81b54
NaveenrajV <naveenraj436@gmail.com>
2019-09-25 17:38:24 +0530

    Added route for  unknown paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route
+} from "react-router-dom";
 import ProtectedRoute from "./components/HOC/ProtectedRoute";
 import Details from "./components/Details/Details";
 import Chat from "./components/Chat/Chat";
@@ -9,6 +14,7 @@ import Signup from "./components/signup/signup";
 import chatIcon from "./assets/chatBubble.svg";
 import closeIcon from "./assets/close1.png";
 import "./App.css";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
 
 function App(props) {
   let [displayChat, setToggle] = useState(false);
@@ -44,6 +50,8 @@ function App(props) {
             logged={props.isLogged}
             component={Details}
           />
+          <Route path="/error" component={ErrorPage} />
+          <Redirect from="*" to="/error" />
         </Switch>
         <Chat show={displayChat} />
         <div className="Icon" onClick={e => setToggle(prevState => !prevState)}>
